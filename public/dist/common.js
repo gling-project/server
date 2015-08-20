ea6c7082dd8c977ebd8aed955efe22a27552327e
florian <florian.jeanmart@factorx.eu>
2015-08-20 18:22:09 +0200

    pre-deploy

--- public/dist/common.js
+++ public/dist/common.js
@@ -3256,19 +3256,19 @@ myApp.service("$flash", ['$filter', function($filter) {
     }
 }]);
 
-//myApp.service("generateId", function($rootScope) {
-//    this.generate = function() {
-//        var i, possible, text;
-//        text = "";
-//        possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//        i = 0;
-//        while (i < 20) {
-//            text += possible.charAt(((Math.random() * possible.length)|0));
-//            i++;
-//        }
-//        return text;
-//    };
-//});
+myApp.service("generateId", ['$rootScope', function($rootScope) {
+    this.generate = function() {
+        var i, possible, text;
+        text = "";
+        possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        i = 0;
+        while (i < 20) {
+            text += possible.charAt(((Math.random() * possible.length)|0));
+            i++;
+        }
+        return text;
+    };
+}]);
 
 myApp.service("translationService", ['$rootScope', '$filter', '$http', '$locale', function ($rootScope, $filter, $http,$locale) {
     var svc;
