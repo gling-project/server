efdd7db45537db0de089f4d8b5fb31fae51e85dc
florian <florian.jeanmart@factorx.eu>
2015-09-18 01:23:20 +0200

    pre-deploy

--- app/be/lynk/server/controller/MainController.java
+++ app/be/lynk/server/controller/MainController.java
@@ -97,7 +97,7 @@ public class MainController extends AbstractController {
 
     public Result generateDefaultPage(String url, boolean forceMobile) {
 
-        if (ctx().request().cookie(CommonSecurityController.COOKIE_ALREADY_VISITED) == null && (url == null || url == "")) {
+        if (!isMobileDevice() && !forceMobile && ctx().request().cookie(CommonSecurityController.COOKIE_ALREADY_VISITED) == null && (url == null || url == "")) {
             addAlreadyVisitedCookie();
             return ok(be.lynk.server.views.html.welcome_page.render(getAvaiableLanguage()));
         } else {
