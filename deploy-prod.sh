5f3409f4a8499ec514e2635a8508063971da9a59
florian <florian.jeanmart@factorx.eu>
2015-09-22 18:58:16 +0200

    pre-deploy

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,8 +4,23 @@ read dummyVar
 
 echo "Run grunt"
 grunt
+
+NOW=$(date +"%Y%m%d%H%M")
+sed -i 's/project.lastVersion=".*"/project.lastVersion="'$NOW'"/g' conf/application.conf
+
+for old in public/dist/*.final.js; do
+    new=$(echo $old | sed -e 's/final./'$NOW'.final./')
+    mv -v "$old" "$new"
+  done
+
+for old in public/dist/*.final.css; do
+    new=$(echo $old | sed -e 's/final./'$NOW'.final./')
+    mv -v "$old" "$new"
+  done
+
 echo "rename package.json"
 mv ./package.json ./-package.json
+
 git add -A
 git commit -m "pre-deploy"
 
@@ -14,4 +29,7 @@ echo "Deploy"
 git push heroku-prod master
 
 mv ./-package.json ./package.json
+
+rm -r public/dist/*
+
 
