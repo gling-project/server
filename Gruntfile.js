ab986efacaa50a372d027993a63947529ae49494
florian <florian.jeanmart@factorx.eu>
2015-09-30 11:19:35 +0200

    pre-deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,6 @@ module.exports = function (grunt) {
                         dest: 'public/dist/styleMobile.css',
 
                         src: ['public/components/mobile-angular-ui/dist/css/mobile-angular-ui-base.min.css',
-                            'public/components/font-awesome/css/font-awesome.min.css',
                             'public/components/angular-socialshare/angular-socialshare.css',
                             'public/css/glingicon.css',
                             'target/scala-2.10/resource_managed/main/public/stylesheets/mobile.css'
