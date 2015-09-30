023b8f93f87a42c573fcfb707e17df626512a1cf
florian <florian.jeanmart@factorx.eu>
2015-09-30 13:59:00 +0200

    pre-deploy

--- public/javascripts/directive/form/business/BusinessFormCtrl.js
+++ public/javascripts/directive/form/business/BusinessFormCtrl.js
@@ -48,50 +48,50 @@ myApp.directive('businessFormCtrl', function ($flash, directiveService) {
                             field: scope.getInfo().dto,
                             fieldName: 'vta'
                         },
-                        //description: {
-                        //    name: 'description',
-                        //    fieldTitle: "--.generic.desc",
-                        //    validationRegex: /^[\s\S]{2,1500}$/gi,
-                        //    validationMessage: ['--.generic.validation.size', '2', '1500'],
-                        //    disabled: function () {
-                        //        return scope.getInfo().disabled;
-                        //    },
-                        //    field: scope.getInfo().dto,
-                        //    fieldName: 'description'
-                        //},
+                        description: {
+                            name: 'description',
+                            fieldTitle: "--.generic.desc",
+                            validationRegex: /^[\s\S]{2,1500}$/gi,
+                            validationMessage: ['--.generic.validation.size', '2', '1500'],
+                            disabled: function () {
+                                return scope.getInfo().disabled;
+                            },
+                            field: scope.getInfo().dto,
+                            fieldName: 'description'
+                        },
                         phone: {
                             name: 'phone',
                             fieldTitle: "--.generic.phone",
-                            validationRegex: /^[0-9. *-+/]{6,16}$/,
+                            validationRegex: /^[0-9. *-+\/]{6,16}$/,
                             validationMessage: '--.validation.dto.phone',
                             disabled: function () {
                                 return scope.getInfo().disabled;
                             },
                             field: scope.getInfo().dto,
                             fieldName: 'phone'
+                        },
+                        email: {
+                            name: 'business-email',
+                            fieldTitle: "--.business.contactEmail",
+                            validationRegex: /^($|(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$)/,
+                            validationMessage: '--.validation.dto.notNull',
+                            disabled: function () {
+                                return scope.getInfo().disabled;
+                            },
+                            field: scope.getInfo().dto,
+                            fieldName: 'email'
+                        },
+                        website: {
+                            name: 'website',
+                            fieldTitle: "--.business.website",
+                            validationRegex: /^($|^(http[s]?:\/\/(www\.)?)[0-9A-Za-z-\.@:%_\+~#=]+\.[a-zA-Z]{2,3}((\/|\?).*)?$)/,
+                            validationMessage: '--.validation.dto.url',
+                            disabled: function () {
+                                return scope.getInfo().disabled;
+                            },
+                            field: scope.getInfo().dto,
+                            fieldName: 'website'
                         }
-                        //email: {
-                        //    name: 'business-email',
-                        //    fieldTitle: "--.business.contactEmail",
-                        //    validationRegex: /^($|(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$)/,
-                        //    validationMessage: '--.validation.dto.notNull',
-                        //    disabled: function () {
-                        //        return scope.getInfo().disabled;
-                        //    },
-                        //    field: scope.getInfo().dto,
-                        //    fieldName: 'email'
-                        //},
-                        //website: {
-                        //    name: 'website',
-                        //    fieldTitle: "--.business.website",
-                        //    validationRegex: /^($|^(http[s]?:\/\/(www\.)?)[0-9A-Za-z-\.@:%_\+~#=]+\.[a-zA-Z]{2,3}((\/|\?).*)?$)/,
-                        //    validationMessage: '--.validation.dto.url',
-                        //    disabled: function () {
-                        //        return scope.getInfo().disabled;
-                        //    },
-                        //    field: scope.getInfo().dto,
-                        //    fieldName: 'website'
-                        //}
 
                     };
 
