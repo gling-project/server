38edd0b01424fc67b0951eda0306da644c18e5d7
florian <florian.jeanmart@factorx.eu>
2015-11-13 16:48:15 +0100

    pre-deploy

--- public/dist/mobile.201511131648.final.js
+++ public/dist/mobile.201511131648.final.js
@@ -728,6 +728,140 @@ $(".inject-box").append(directive);return $scope.save=function(share){if(!$scope
 
 }).call(this);
 
+(function() {
+
+  myApp.directive('publicationListMobileCtrl', ['$rootScope', 'businessService', 'geolocationService', 'directiveService', 'searchService', '$location', 'modalService', '$timeout', function($rootScope, businessService, geolocationService, directiveService, searchService, $location, modalService, $timeout) {
+    return {
+      restrict: 'E',
+      scope: directiveService.autoScope({
+        ngInfo: '='
+      }),
+      templateUrl: '/assets/javascripts/directive/component/publicationListMobile/template.html',
+      replace: true,
+      transclude: true,
+      compile: function() {
+        return {
+          post: function(scope) {
+            directiveService.autoScopeImpl(scope);
+            scope.getInfo().loading = true;
+            return scope.$watch('getInfo().data', function() {
+              var publication, _i, _len, _ref, _results;
+              scope.publications = scope.getInfo().data;
+              _ref = scope.publications;
+              _results = [];
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                publication = _ref[_i];
+                publication.descriptionLimit = scope.descriptionLimitBase;
+                _results.push(publication.interval = publication.endDate - (new Date));
+              }
+              return _results;
+            });
+          }
+        };
+      }
+    };
+  }]);
+
+}).call(this);
+
+(function() {
+
+  myApp.directive('publicationListMobileForBusinessCtrl', ['$rootScope', 'businessService', 'geolocationService', 'directiveService', 'searchService', '$timeout', function($rootScope, businessService, geolocationService, directiveService, searchService, $timeout) {
+    return {
+      restrict: 'E',
+      scope: directiveService.autoScope({
+        ngInfo: '='
+      }),
+      templateUrl: '/assets/javascripts/directive/component/publicationListMobileForBusiness/template.html',
+      replace: true,
+      transclude: true,
+      compile: function() {
+        return {
+          post: function(scope) {
+            directiveService.autoScopeImpl(scope);
+            scope.descriptionLimitBase = 250;
+            scope.currentPage = 0;
+            scope.allLoaded = false;
+            scope.loadSemaphore = false;
+            scope.publications = [];
+            scope.loading = false;
+            scope.isArchived = function(publication) {
+              return publication.endDate < (new Date).getTime();
+            };
+            $('.scrollable-content-body').on('scroll', function() {
+              var scrollBottom;
+              scrollBottom = $('.scrollable-content-body').scrollTop() + $('.scrollable-content-body').height();
+              if ($('.scrollable-content-inner').height() - scrollBottom < 200) {
+                if (scope.loadSemaphore === false) {
+                  console.log('-- SERACH FROM SCROOL');
+                  scope.loadSemaphore = true;
+                  scope.currentPage = scope.currentPage + 1;
+                  return scope.search();
+                }
+              }
+            });
+            scope.openGallery = function(image, publication) {
+              return $rootScope.$broadcast('DISPLAY_PICTURE_IN_GALLERY', {
+                list: publication.pictures,
+                first: image
+              });
+            };
+            scope.getInterestClass = function(publication) {
+              if (publication.interest !== null) {
+                return 'gling-icon-' + publication.interest.name;
+              }
+              return null;
+            };
+            scope.getInfo().refresh = function(type) {
+              scope.currentPage = 0;
+              scope.publications = [];
+              scope.type = type;
+              return scope.search();
+            };
+            scope.success = function(data) {
+              var d, publication, _i, _j, _len, _len1, _ref;
+              if (scope.currentPage === 0) {
+                scope.publications = [];
+              }
+              if (data.length === 0) {
+                scope.allLoaded = true;
+              }
+              scope.loadSemaphore = false;
+              for (_i = 0, _len = data.length; _i < _len; _i++) {
+                d = data[_i];
+                scope.publications.push(d);
+              }
+              _ref = scope.publications;
+              for (_j = 0, _len1 = _ref.length; _j < _len1; _j++) {
+                publication = _ref[_j];
+                publication.descriptionLimit = scope.descriptionLimitBase;
+                publication.interval = (publication.endDate - (new Date)) / 1000;
+              }
+              $timeout((function() {
+                if (scope.getInfo().scrollTo !== null) {
+                  $('.main-body').scrollTop($('#publication' + scope.getInfo().scrollTo).offset().top);
+                  return scope.$apply();
+                }
+              }), 1);
+              return scope.loading = false;
+            };
+            scope.search = function() {
+              if (scope.allLoaded === true) {
+                return;
+              }
+              scope.loading = true;
+              return searchService.byBusiness(scope.currentPage, scope.getInfo().businessId, scope.success);
+            };
+            console.log('-- SERACH FROM initialization');
+            return scope.search();
+          }
+        };
+      }
+    };
+  }]);
+
+}).call(this);
+
 myApp.directive("headerSearchCtrl", ['$rootScope', '$location', '$timeout', 'modalService', function ($rootScope,$location,$timeout,modalService) {
     return {
         restrict: "E",
